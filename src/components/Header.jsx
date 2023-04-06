e4c9ddf683ae6b47958a08bce46b7a5b89f985df
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-04-06 09:28:02 +0430

    the importance of Callbackl

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+function Header({ text, bgColor, txtColor }) {
+
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: txtColor
+    }
+
+    return (
+        <header style={headerStyles}>
+            <div className='container'  >
+                <h2>{text}</h2>
+            </div>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    text: "Feedback UI",
+    bgColor: 'rgba(0,0,0,0.4)',
+    txtColor: "#ff6a95"
+}
+Header.propTypes = {
+    text: PropTypes.string.isRequired,
+    bgColor: PropTypes.string,
+    txtColor: PropTypes.string
+}
+
+export default Header

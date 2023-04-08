42e0bcce9b1137f0ad1484a7d59d51ba1a64b157
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-04-08 13:54:17 +0430

    adding animation and add Function

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
+import { motion, AnimatePresence } from 'framer-motion'
 function FeedbackList({ feedback, handleDelete }) {
 
     if (!feedback || feedback.length === 0) {
@@ -9,13 +10,22 @@ function FeedbackList({ feedback, handleDelete }) {
 
     return (
         <div className='feedback-list'>
-            {feedback.map((item) => (
-                <FeedbackItem
-                    item={item}
-                    key={item.id}
-                    handleDelete={handleDelete}
-                />
-            ))}
+            <AnimatePresence>
+                {feedback.map((item) => (
+                    <motion.div
+                        key={item.id}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                    >
+                        <FeedbackItem
+                            item={item}
+                            key={item.id}
+                            handleDelete={handleDelete}
+                        />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
         </div>
     )
 }
